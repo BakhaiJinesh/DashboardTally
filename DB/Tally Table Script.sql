20824814b92a4da3f0c8578369e3964872affad0
Bakhai Jinesh <125656564+BakhaiJinesh@users.noreply.github.com>
2024-10-10 13:30:02 +0530

    Update Tally Table Script.sql

--- DB/Tally Table Script.sql
+++ DB/Tally Table Script.sql
@@ -20,6 +20,7 @@ CREATE TABLE Users (
     UserName VARCHAR(100) NOT NULL, -- Name of the user
     Email VARCHAR(150) UNIQUE NOT NULL, -- Email of the user
     Password VARCHAR(255) NOT NULL, -- Password for authentication
+    LevelAccess INT NOT NULL,
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the user was created
 );
 
